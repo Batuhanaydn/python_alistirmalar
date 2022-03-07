f8f37ce4a1b56575d05b818efa36f8f6b3564ab2
Muhammed Batuhan Aydın <56546166+Batuhanaydn@users.noreply.github.com>
2022-03-07 12:56:37 +0300

    Different perspective
    mr. sinan I apologize if you are teaching. I hope I could add a different perspective to the question. I wish you good lessons

--- Stringler/lab_4.py
+++ Stringler/lab_4.py
@@ -1,8 +1,17 @@
 import re
-s = input('Type a word: ')
-slower=''.join(re.findall(r'[a-z]',s))
-supper=''.join(re.findall(r'[A-Z]',s))
-if len(supper) == 0:
-    print('The entered word(s) are made up of lowercase letters.')
-else:
-    print('Word(s) has capital letters')
+import string
+import random
+def findUpper(s):
+    
+    slower=''.join(re.findall(r'[a-z]',s))
+    supper=''.join(re.findall(r'[A-Z]',s))
+    if len(supper) == 0:
+        print('The entered word(s) are made up of lowercase letters.')
+    else:
+        print('Word(s) has capital letters')
+
+if __name__ == "__main__":
+    length = 10
+    s = ''.join(random.choices(string.ascii_letters, k=length))
+    # print(s)
+    findUpper(s)
